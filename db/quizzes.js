70ff0480a98e20fce6371ef6667445bff6c3d54a
Abhishek Verma <abhishekkumarverma18@gmail.com>
2024-11-01 15:20:19 +0530

    added another quiz

--- db/quizzes.js
+++ db/quizzes.js
@@ -1,3 +1,13 @@
+import geog from "../assets/geog.svg";
+import history from "../assets/history.svg";
+import math from "../assets/maths.svg";
+import book from "../assets/book.svg";
+import comics from "../assets/comics.svg";
+import science from "../assets/science.svg";
+import tech from "../assets/tech.svg";
+import sports from "../assets/sports.svg";
+import vehicle from "../assets/vehicle.svg";
+
 const { v4: uuid } = require("uuid")
 
 const quizzes = {
@@ -106,6 +116,55 @@ const quizzes = {
                 },
             ]
         },
+        {
+            id: uuid(),
+            category: "education",
+            img: math,
+            title: "Mathematics",
+            description: "Do you believe you are a maths genius??? Test your skills to check if you actually are a genius or a noob.",
+            quiz: [
+                {
+                    id: uuid(),
+                    question: "What was Wonder Woman originally named?",
+                    options: [
+                        { id: uuid(), option: "Anita", isCorrect: false },
+                        { id: uuid(), option: "Suprema", isCorrect: true },
+                        { id: uuid(), option: "Mishita", isCorrect: false },
+                        { id: uuid(), option: "None", isCorrect: false }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "Which supervillain was once the Iranian Ambassador to the U.N.?",
+                    options: [
+                        { id: uuid(), option: "Wonder Man", isCorrect: false },
+                        { id: uuid(), option: "Superman", isCorrect: false },
+                        { id: uuid(), option: "The Joker", isCorrect: true },
+                        { id: uuid(), option: "Batman", isCorrect: false }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "What is batman's favorite food?",
+                    options: [
+                        { id: uuid(), option: "Chicken Salad", isCorrect: false },
+                        { id: uuid(), option: "Turkey Roast", isCorrect: false },
+                        { id: uuid(), option: "Goat Ribbs", isCorrect: false },
+                        { id: uuid(), option: "Mulligatawny soup", isCorrect: true }
+                    ]
+                },
+                {
+                    id: uuid(),
+                    question: "In how many seconds can Green Arrow shoot and reload?",
+                    options: [
+                        { id: uuid(), option: "Two and a half seconds", isCorrect: true },
+                        { id: uuid(), option: "Two seconds", isCorrect: false },
+                        { id: uuid(), option: "One seconds", isCorrect: false },
+                        { id: uuid(), option: "One and a half seconds", isCorrect: false }
+                    ]
+                },
+            ]
+        }
 
     ]
 }
